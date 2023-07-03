08e2192484bb8753a143ab0545c6da05b7e3865c
RishavSht <np03cs4s220093@heraldcollege.edu.np>
2023-07-03 17:19:31 +0545

    some changes in signup.ejs and app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -20,3 +22,4 @@ mongoose.connect("mongodb://localhost:27017/node-auth", {
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
