8f8a4df6d9b4e77547faaba730c46f6d85332aab
RishavSht <np03cs4s220093@heraldcollege.edu.np>
2023-07-03 16:07:55 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require("./routes/authRoutes")
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+
+// database connection
+mongoose.connect("mongodb://localhost:27017/node-auth", {
+}).then((result) => app.listen(31))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
